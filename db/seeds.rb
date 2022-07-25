1bd174835f76c404d301820dc7744985c87fbefd
JawaraGordon <jawaragordon@icloud.com>
2022-07-24 21:59:25 -0400

    commit tree 2ad9a14921475fb35ced9b028c73cdb4209ac0ec

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+u1 = User.create(username:'ix',  password_digest:"123")
+u2 = User.create(username:'rose', password_digest:"123")
+
+u3 = User.create(username:'Mikhail', password_digest:"123")
+u4 = User.create(username:'Jacques',  password_digest:"123")
+u5 = User.create(username:'Florrie',  password_digest:"123")
+u6 = User.create(username:'Eliott',  password_digest:"123")
+puts "Complete"
+
